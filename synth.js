997cd37292c68cc45ec20976342809cd72f94c60
dcs0spb <s.p.bradley@durham.ac.uk>
2019-04-03 00:20:27 +0800

    Thought this was already done

--- synth.js
+++ synth.js
@@ -18,12 +18,12 @@ module.exports = function (RED) {
   // exponential scale with 0->0 and 100->1
   function volume2amp (node) {
     let volume = Math.max(0, node.volume);
-    
+
     const globalVolume = node.context().global.get('volume');
     if (globalVolume !== null && globalVolume >= 0) {
       volume = volume * globalVolume / 100;
     }
-    
+
     const base = 1.02;
     return (Math.pow(base, volume) - 1) / (Math.pow(base, 100) - 1);
   }
