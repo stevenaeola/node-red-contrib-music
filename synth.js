2dac7a3c554f4bc4b391e4f175d58229110cf8d8
dcs0spb <s.p.bradley@durham.ac.uk>
2019-04-23 20:18:01 +0100

    Added whole tone and octatonic scales. closes #71

--- synth.js
+++ synth.js
@@ -373,7 +373,9 @@ module.exports = function (RED) {
                 'major pentatonic': [0, 2, 4, 7, 9, 12],
                 'minor pentatonic': [0, 3, 5, 7, 10, 12],
                 blues: [0, 3, 5, 6, 7, 10, 12],
-                chromatic: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+                chromatic: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                'whole tone': [0, 2, 4, 6, 8, 10, 12],
+                'octatonic': [0, 2, 3, 5, 6, 8, 9, 11, 12]
             };
 
             if (!intervals[scale]) {
