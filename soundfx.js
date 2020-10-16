79be4625d94e9ae1cadfba996e8f2309f6f2f95d
dcs0spb <s.p.bradley@durham.ac.uk>
2020-10-16 18:02:51 +0100

    Unnecessary freeSynth (v1 style) removed

--- soundfx.js
+++ soundfx.js
@@ -66,7 +66,6 @@ module.exports = function (RED) {
         });
 
         this.on('close', function () {
-            sc.freeSynth(node, node.synthID);
             node.synthID = null;
         });
 
