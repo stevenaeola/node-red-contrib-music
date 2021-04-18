ac2f78b8342326d87e66c2e8ff9ee876f51e8613
dcs0spb <s.p.bradley@durham.ac.uk>
2021-04-18 12:31:57 +0100

    Start move to jest testing

--- test/sequencer_spec.js
+++ test/sequencer_spec.js
@@ -1,4 +1,3 @@
-const sinon = require('sinon');
 const helper = require('node-red-node-test-helper');
 const sequencerNode = require('../sequencer.js');
 
@@ -43,7 +42,7 @@ describe('sequencer Node', function () {
         helper.stopServer(done);
     });
 
-    it('should be loaded', function (done) {
+    test('should be loaded', function (done) {
         let flow = sequencerBase;
         helper.load(sequencerNode, flow, function () {
             const n1 = helper.getNode('n1');
@@ -56,23 +55,69 @@ describe('sequencer Node', function () {
         });
     });
 
-    it('should send first beat unchanged when first length is 1 and bar is started', function (done) {
-        let flow = sequencerBase;
-        helper.load(sequencerNode, flow, function () {
-            const n1 = helper.getNode('n1');
-            const n2 = helper.getNode('n2');
-            const spy = sinon.spy();
-            const injectMsg = { 'payload': 'tick', 'start': ['beat', 'bar'] };
-            n2.on('input', function (msg) {
-                try {
-                    spy(injectMsg);
-                    done();
-                } catch (err) {
-                    done(err);
-                }
-            });
-            n1.receive(injectMsg);
-            spy.should.have.been.called;
-        });
-    });
+    // it('should send first beat when first length is 1 and bar is started', function (done) {
+    //     let flow = sequencerBase;
+    //     helper.load(sequencerNode, flow, function () {
+    //         const n1 = helper.getNode('n1');
+    //         const n2 = helper.getNode('n2');
+    //         const spy = sinon.spy();
+    //         const injectMsg = { 'payload': 'tick', 'start': ['beat', 'bar'] };
+    //         n2.on('input', function (msg) {
+    //             try {
+    //                 spy(msg);
+    //             } catch (err) {
+    //                 done(err);
+    //             }
+    //         });
+    //         spy.should.not.have.been.called;
+    //         n1.receive(injectMsg);
+    //         spy.should.have.been.called;
+    //         done();
+    //     });
+    // });
+
+    // it('should not send beat when start event (bar) has not happened', function (done) {
+    //     let flow = sequencerBase;
+    //     helper.load(sequencerNode, flow, function () {
+    //         const n1 = helper.getNode('n1');
+    //         const n2 = helper.getNode('n2');
+    //         const spy = sinon.spy();
+    //         const injectMsg = { 'payload': 'tick', 'start': ['beat'] };
+    //         n2.on('input', function (msg) {
+    //             try {
+    //                 spy(msg);
+    //             } catch (err) {
+    //                 done(err);
+    //             }
+    //         });
+    //         n1.receive(injectMsg);
+    //         spy.should.not.have.been.called;
+    //         done();
+    //     });
+    // });
+
+    // it('should follow pattern from start event', function (done) {
+    //     let flow = sequencerBase;
+    //     helper.load(sequencerNode, flow, function () {
+    //         const n1 = helper.getNode('n1');
+    //         const n2 = helper.getNode('n2');
+    //         const spy = sinon.spy();
+    //         const injectMsg1 = { 'payload': 'tick', 'start': ['beat', 'bar'] };
+    //         const injectMsg2 = { 'payload': 'tick', 'start': ['bar'] };
+    //         n2.on('input', function (msg) {
+    //             try {
+    //                 spy(msg);
+    //             } catch (err) {
+    //                 done(err);
+    //             }
+    //         });
+    //         n1.receive(injectMsg1);
+    //         spy.should.have.been.calledTwice;
+    //         n1.receive(injectMsg2);
+    //         spy.should.have.been.calledTwice;
+    //         n1.receive(injectMsg2);
+    //         spy.should.have.been.calledTwice;
+    //         done();
+    //     });
+    // });
 });
